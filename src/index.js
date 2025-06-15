db1411829f56268c3fef37d1bb60158ed16b52a3
Cynthia Mugo <cynthiamugo25@gmail.com>
2025-06-15 22:22:30 +0300

    Add delete functionality for guest list

--- src/index.js
+++ src/index.js
@@ -1,23 +1,48 @@
 document.addEventListener('DOMContentLoaded', () => {
     const form = document.getElementById("guest-form");
     const input = document.getElementById("guest-name"); //Name of the guest input field
-    const submit = document.getElementById("add-guest"); //Button to add the guest
-        submit.addEventListener("click", (event) => {
+
+    // console.log(input);
+    // console.log(form);
+
+    // Add an event listener to the form to handle the submission
+        form.addEventListener("submit", (event) => {
             event.preventDefault();
 
             const guestName = input.value.trim(); // Get the trimmed value of the input field
-            if (guestName === "") {
+            if (guestName === "") { //For empty input
                 alert("Please enter a guest name.");
                 return;
             }
             handleClick(guestName);
 
-            form.reset();
+            const guestList = document.getElementById("guest-list"); // The list where guests will be added
+            // Limit to 10 guests
+
+
+            form.reset(); 
     })
     const handleClick = (guestName) => {
         const addGuest = document.getElementById("guest-list"); // The list where guests will be added
         const parItem = document.createElement("p");
-        parItem.textContent = guestName;
+        parItem.textContent = `${guestName} `; // Create a new paragraph element with the guest name
         addGuest.appendChild(parItem);
+        parItem.style.fontSize = "20px";
+
+        //remove a guest from the list using a delete button.
+        const deleteBtn = document.createElement("button");
+        deleteBtn.textContent = "Delete";
+        parItem.appendChild(deleteBtn);
+        // style the delete button
+        deleteBtn.style.backgroundColor = "red";
+        deleteBtn.style.color = "white";
+        deleteBtn.style.border = "none";
+        deleteBtn.style.padding = "5px 10px";
+        deleteBtn.style.borderRadius = "5px";
+
+        // Remove the paragraph element from the list
+        deleteBtn.addEventListener("click", () => {
+            addGuest.removeChild(parItem); 
+        });
     };
 });
