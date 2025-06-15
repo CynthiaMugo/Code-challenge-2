12a08ee379d2898c0a893519296b76f2bd3f5cf9
Cynthia Mugo <cynthiamugo25@gmail.com>
2025-06-15 21:33:32 +0300

    Refactor guest list rendering and input handling in JavaScript

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const submit = document.getElementById("add-guest");
-    submit.addEventListener("click", (event) => {
-        event.preventDefault();
+    const form = document.getElementById("guest-form");
+    const input = document.getElementById("guest-name"); //Name of the guest input field
+    const submit = document.getElementById("add-guest"); //Button to add the guest
+        submit.addEventListener("click", (event) => {
+            event.preventDefault();
+
+            const guestName = input.value.trim(); // Get the trimmed value of the input field
+            if (guestName === "") {
+                alert("Please enter a guest name.");
+                return;
+            }
+            handleClick(guestName);
+
+            form.reset();
     })
+    const handleClick = (guestName) => {
+        const addGuest = document.getElementById("guest-list"); // The list where guests will be added
+        const parItem = document.createElement("p");
+        parItem.textContent = guestName;
+        addGuest.appendChild(parItem);
+    };
 });
